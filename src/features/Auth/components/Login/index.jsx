d8bbc564ddc8f5f3e324434d435eb34cff2ae060
khanh <baokhanhtlc1902@gmail.com>
2021-05-29 10:22:11 +0700

    fix redirect login

--- src/features/Auth/components/Login/index.jsx
+++ src/features/Auth/components/Login/index.jsx
@@ -2,19 +2,21 @@ import { unwrapResult } from '@reduxjs/toolkit';
 import { login } from 'features/Auth/userSlice';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 import LoginForm from '../LoginForm';
 
 Login.propTypes = {};
 
 function Login(props) {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleSubmit = async (value) => {
     try {
       const action = login(value);
       const resultAction = await dispatch(action);
       const user = unwrapResult(resultAction);
-      window.location = 'http://localhost:3000/admin';
+      history.push('admin');
 
       console.log('new user', user);
     } catch (error) {
